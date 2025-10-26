0ee58266b43226795fd2638cccbadb2fa3cd7a70
Zhangtingwei-2201 <726661588@qq.com>
2025-10-26 17:11:11 +0800

    Update Chapter7.cpp

--- opencvlearning/Chapter7.cpp
+++ opencvlearning/Chapter7.cpp
@@ -8,31 +8,23 @@ using namespace cv;
 using namespace std;
 
 void getContours(Mat imgDil, Mat img) {
-
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
-
 	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
-
 	vector<vector<Point>> conPoly(contours.size());
 	vector<Rect> boundRect(contours.size());
-	 
 	for (int i = 0; i < contours.size(); i++)
 	{
 		int area = contourArea(contours[i]);
 		cout << area << endl;
 		string objectType;
-
 		if (area > 1000) 
 		{
 			float peri = arcLength(contours[i], true);
 			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
 			cout << conPoly[i].size() << endl;
 			boundRect[i] = boundingRect(conPoly[i]);
-		
 			int objCor = (int)conPoly[i].size();
-
 			if (objCor == 3) { objectType = "Tri"; }
 			else if (objCor == 4)
 			{ 
@@ -42,7 +34,6 @@ void getContours(Mat imgDil, Mat img) {
 				else { objectType = "Rect";}
 			}
 			else if (objCor > 4) { objectType = "Circle"; }
-
 			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
 			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
 			putText(img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_PLAIN,1, Scalar(0, 69, 255), 2);
@@ -50,24 +41,17 @@ void getContours(Mat imgDil, Mat img) {
 	}
 }
 
-
-
 int main() {
 	string path = "Resources/shapes.png";
 	Mat img = imread(path);
 	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
-
-	// Preprocessing
 	cvtColor(img, imgGray, COLOR_BGR2GRAY);
 	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
 	Canny(imgBlur, imgCanny, 25, 75);
 	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
 	dilate(imgCanny, imgDil, kernel);
-
 	getContours(imgDil,img);
-
 	imshow("Image", img);
 	waitKey(0);
-
     return 0;
 }
